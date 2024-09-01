f8bc8e68693b1a6fe720c97c01d5fd32d14fb5c4
Omair-D <odurrani@kent.edu>
2024-09-01 13:22:56 -0400

    Forging timer skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+
 function App() {
   return (
-    <div>
-      
+    <div className="container">
+        <div className = 'Lead'>
+          <h2 className='lead-2'>
+            Animedoro Timer
+
+          </h2>
+
+
+        </div>
+        <div className='Timer'>
+          <h2>40:00</h2>
+
+        </div>
     </div>
   );
 }
