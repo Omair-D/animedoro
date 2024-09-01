461e870b707f1daddd273229c89e3571cb8f3544
Omair-D <odurrani@kent.edu>
2024-09-01 13:42:25 -0400

    Added block buttons (start/pause)

--- src/App.js
+++ src/App.js
@@ -4,20 +4,25 @@ import { Button } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
+      <div className="d-flex align-items-center flex-column">
         <div className = 'Lead'>
-          <h2 className='lead-2'>
+          <h2 className='display-2'>
             Animedoro Timer
-
           </h2>
-
-
         </div>
-        <div className='Timer'>
-          <h2>40:00</h2>
 
+        <div className='Timer py-4 my-2'>
+          <h2 className='display-1 align-self-center'>40:00</h2>
         </div>
     </div>
+    <div className='Ctrl py-6 my-2 d-grid gap-2'>
+            <Button variant="dark" size ="lg">Start</Button>
+            <Button variant="light" size ="lg">Pause</Button>
+        </div> 
+  </div>
+
+
   );
 }
 
