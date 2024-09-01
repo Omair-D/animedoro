ea1cf91107dfde24163fa4f965757c5a8f8dbcd4
Omair-D <odurrani@kent.edu>
2024-09-01 13:52:17 -0400

    Added Reset Button

--- src/App.js
+++ src/App.js
@@ -16,10 +16,11 @@ function App() {
           <h2 className='display-1 align-self-center'>40:00</h2>
         </div>
     </div>
-    <div className='Ctrl py-6 my-2 d-grid gap-2'>
-            <Button variant="dark" size ="lg">Start</Button>
-            <Button variant="light" size ="lg">Pause</Button>
-        </div> 
+    <div className='Ctrl py-2 my-10 d-grid gap-2 fixed-bottom'>
+      <Button variant="dark" size ="lg">Start</Button>
+      <Button variant="secondary" size ="lg">Reset</Button>
+      <Button variant="light" size ="lg">Pause</Button>
+    </div> 
   </div>
 
 
